d38898658d7ed72899bfdf26dcc162717a3502a1
shahriarnaiem-rafi <shahriarnaiemrafi33@gmail.com>
2024-10-24 10:04:52 +0600

    arrays 10 method

---
+++ Array.js
@@ -0,0 +1,53 @@
+var arr=[45,76,87,99,23];
+// sort()   it use for serially print
+//console.log(arr.sort());
+
+/*  //reverse()  it is use for reverse the array
+    console.log(arr.reverse());
+*/
+
+/*    //push() is use for insert a data in last;
+    //arr.push("Hello");
+*/
+
+ /*    //pop() is use for remove a data in last;
+    //arr.pop();
+*/
+/*
+    //unshift() is use for insert a data in first;
+    //arr.unshift(33);
+*/ 
+
+/*
+    //shift() is use for remove a data in first;
+    //arr.shift();
+*/
+/*
+    //splice() is use  for remote [1]data to [2]data ad also insert  
+//arr.splice(1,2,"hello");
+*/
+
+// for output :- console.log(arr);
+
+
+/*
+    //slice() its get a new array from the old array [1]data to [3]data and its take the data which is before the end data 
+    // /var arr2=arr.slice(1,3);
+     // for output :- console.log(arr2);
+*/
+
+
+/**
+     //concat()  add another array in the old array
+    // const arr3=[34,54,67,34];
+    // const arr4=[334,443,676,434];
+    // console.log(arr3.concat(arr4));
+
+ */
+
+/*
+        //join() it used in the place of [,] in array
+    // console.log(arr3.join("&"));
+
+*/
+
